6fdc399179a8d4c2f499849aa6a1c9e813266d94
paulsmithkc <paulsmithkc@gmail.com>
2015-02-08 13:44:12 -0600

    Adding local copy of mse

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
-set 7Z=C:\Program Files\7-Zip
-set MSE=C:\Program Files (x86)\Magic Set Editor 2
+rem set 7Z=C:\Program Files\7-Zip
+rem set MSE=C:\Program Files (x86)\Magic Set Editor 2
+set MSE=%~dp0\mse
 
 xcopy /i /e /s /y "shadowrun.mse-game" "%MSE%\data\shadowrun.mse-game\"
 xcopy /i /e /s /y "shadowrun-standard.mse-style" "%MSE%\data\shadowrun-standard.mse-style\"
