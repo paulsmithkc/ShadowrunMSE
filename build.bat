5c6c8ad905119946b12e76564603113c160426d7
paulsmithkc <paulsmithkc@gmail.com>
2015-02-08 20:10:22 -0600

    Adding a layout for spell cards

--- build.bat
+++ build.bat
@@ -4,7 +4,9 @@ set MSE=%~dp0\mse
 
 xcopy /i /e /s /y "shadowrun.mse-game" "%MSE%\data\shadowrun.mse-game\"
 xcopy /i /e /s /y "shadowrun-standard.mse-style" "%MSE%\data\shadowrun-standard.mse-style\"
-"%MSE%\mse.exe" --create-installer shadowrun.mse-installer shadowrun.mse-game shadowrun-standard.mse-style
+xcopy /i /e /s /y "shadowrun-spell.mse-style" "%MSE%\data\shadowrun-spell.mse-style\"
+"%MSE%\mse.exe" --create-installer shadowrun.mse-installer shadowrun.mse-game shadowrun-standard.mse-style shadowrun-spell.mse-style
 "%MSE%\mse.exe" shadowrun.mse-installer
+"%MSE%\mse.exe"
 
-pause
+timeout 30
